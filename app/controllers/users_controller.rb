c90d24b468a7c9ffc8c2e2a1ed74502e25b8addb
Iwallis <theiainwallis@gmail.com>
2017-11-30 11:57:56 -0800

    added user/show page with gravatar profile picture

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,18 @@
 class UsersController < ApplicationController
   def new
   end
+
+  def show
+    @user = User.find(params[:id])
+    # uncomment below to have a debugging console added when using server
+    # debugger
+    #
+    # ex:
+    # (byebug) @user.name
+    # "Example User"
+    # (byebug) @user.email
+    # "example@railstutorial.org"
+    # (byebug) params[:id]
+    # "1"
+  end
 end
