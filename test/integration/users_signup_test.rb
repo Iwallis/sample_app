b6fa967a13191df5b30c61dee0e2e81aba3799a5
Iwallis <theiainwallis@gmail.com>
2017-11-30 15:17:16 -0800

    finished user signup

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -11,4 +11,27 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     end
     assert_template 'users/new'
   end
+
+  test "valid signup information" do
+    get signup_path
+    # tests the difference between user.count before and after submitting form
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { name:  "Example User",
+                                         email: "user@example.com",
+                                         password:              "password",
+                                         password_confirmation: "password" } }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+  end
+
+  test "valid success flash message" do
+    get signup_path
+    post users_path, params: { user: { name:  "Example User",
+                                       email: "user@example.com",
+                                       password:              "password",
+                                       password_confirmation: "password" } }
+    follow_redirect!
+    assert_not flash[:success].blank?, "no success flash message"
+  end
 end
