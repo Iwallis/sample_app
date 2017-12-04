94949e780c5e8190508ed8c19222a8425365050f
Iwallis <theiainwallis@gmail.com>
2017-12-03 16:01:08 -0800

    Added password reset email

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
   get 'sessions/new'
 
   get 'users/new'
@@ -17,5 +21,6 @@ Rails.application.routes.draw do
   resources :users
   # the url the activation email will send
   resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
